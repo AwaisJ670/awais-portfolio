54eaacfb995ada55c90ae2e723b3d62ec494abfd
Malik Awais Javaid Iqbal <malikawaisj670@gmail.com>
2025-02-15 11:10:43 +0500

    Fixing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,10 +8,12 @@ const Footer = () => {
     return (
       <footer className="w-full pt-20 pb-10" id="contact">
         <div className="w-full absolute left-0 -bottom-72 min-h-96">
-          <img
+          <Image
             src="/footer-grid.svg"
             alt="grid"
-            className="w-full h-full opacity-50 "
+            width={800}
+            height={800}
+            className="w-full h-full opacity-50"
           />
         </div>
   
